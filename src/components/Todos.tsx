f04042f3e6e0f27e19036cebfff77531741decd8
rruiz88 <109550060+rruiz88@users.noreply.github.com>
2023-05-10 13:18:59 -0500

    Add files via upload

---
+++ src/components/Todos.tsx
@@ -0,0 +1,25 @@
+import { Fragment, useContext } from "react";
+
+import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
+  return (
+    <Fragment>
+      <ul className={classes.todos}>
+        {todosCtx.items.map((item) => (
+          <TodoItem
+            key={item.id}
+            text={item.text}
+            onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default Todos;
